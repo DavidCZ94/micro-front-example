78ab4409e3d81805cf472093506f3336ca63b9c8
DavidCZ94 <david940103@gmail.com>
2024-04-06 18:03:52 -0500

    process.env configurated

--- shell/vite.config.ts
+++ shell/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from "@originjs/vite-plugin-federation";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const proxyTarget = dotenv.config();
-
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -16,8 +10,8 @@ export default defineConfig(() => {
         federation({
           name: 'host-app',
           remotes: {            
-            mfReactA: proxyTarget?.parsed?.VITE_MICROFRONT_REACT_A_URL || '',
-            mfReactB: proxyTarget?.parsed?.VITE_MICROFRONT_REACT_B_URL || '',
+            mfReactA: process.env.VITE_MICROFRONT_REACT_A_URL || '',
+            mfReactB: process.env.VITE_MICROFRONT_REACT_B_URL || '',
           },
           shared: ['react','react-dom'] 
         })
@@ -30,30 +24,3 @@ export default defineConfig(() => {
     }
   }
 })
-
-/* {
-  plugins: [
-      react(),
-      federation({
-        name: 'host-app',
-        remotes: {
-          mfReactA:
-          process.env.NODE_ENV
-          ? "https://micro-front-example-mf-react-1-6222.vercel.app/assets/remoteEntry.js"
-          : "https://micro-front-example-mf-react-1-6222.vercel.app/assets/remoteEntry.js",
-          mfReactB: "http://localhost:5002/assets/remoteEntry.js",
-        },
-        shared: ['react','react-dom'] 
-      })
-  ],
-  define: {
-    'process.env.APP_ENV': JSON.stringify(env.VITE_APP_ENV),
-    'process.env.API_URL': JSON.stringify(env.VITE_API_URL),
-  },
-  build: {
-    target: 'esnext'
-  },
-  server: {
-    port: 5000
-  }
-}) */
